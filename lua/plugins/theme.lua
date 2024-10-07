226f318f0eebce6b255028fe6fe68017bd902f2d
ShafayetStuff <mdshafayatislamifaz13@gmail.com>
2024-10-07 16:10:10 +0600

    new file:   .gitignore 	new file:   .neoconf.json 	new file:   LICENSE 	new file:   init.lua 	new file:   lazy-lock.json 	new file:   lazyvim.json 	new file:   lua/config/autocmds.lua 	new file:   lua/config/cord.lua 	new file:   lua/config/keymaps.lua 	new file:   lua/config/lazy.lua 	new file:   lua/config/options.lua 	new file:   lua/plugins/cord.lua 	new file:   lua/plugins/plugin.lua 	new file:   lua/plugins/theme.lua 	new file:   stylua.toml

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,19 @@
+return{
+{ "bluz71/vim-moonfly-colors",
+name = "moonfly",
+lazy = false,
+priority = 1000 
+},
+{
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+},
+{
+  "scottmckendry/cyberdream.nvim",
+  name ="cyberdream",
+  lazy = false,
+  priority = 1000,
+}
+}
